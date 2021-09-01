4300ca7e93ead2b0743916067fe67850071401d9
전해윤 <junhaeyoon@gmail.com>
2021-09-01 16:39:04 +0900

    Fix: 계좌번호 생성 시 - 제거

--- src/lib/uuid.ts
+++ src/lib/uuid.ts
@@ -1,5 +1,5 @@
 export default (): string => {
-  const uuid: string = '002-xxxx-xxxx-xx'.replace(/x/g, (c) => {
+  const uuid: string = '002xxxxxxxxxx'.replace(/x/g, (c) => {
     // 002는 Doss-Bank 고유 번호임
     const r = (Math.random() * 10) | 0;
     const v = c === 'x' ? r : (r & 3) | 8;
