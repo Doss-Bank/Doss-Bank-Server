dbbb2535cd94b9fece148cb6634b0f01fd4581cb
전해윤 <junhaeyoon@gmail.com>
2021-09-23 17:26:14 +0900

    Fix: docs response 제거(추후 수정 필요)

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -10,14 +10,9 @@ import {
   ApiBearerAuth,
   ApiOperation,
   ApiTags,
-  ApiResponse,
-  ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
 import User from 'src/entities/User';
-import { BaseResponse } from 'src/lib/response/BaseResponse';
-import { UnauthorizedResponse } from 'src/lib/response/errResponse/Unauthorization';
-import { LoginResponse } from 'src/lib/response/User/LoginResponse';
-import { UserResponse } from 'src/lib/response/User/UserResponse';
+import BaseResponse from 'src/lib/response/BaseResponse';
 import { Token } from 'src/lib/token';
 import { PasswordGuard } from 'src/middleware/authMiddleware';
 import LoginDto from './dto/loginDto';
@@ -27,21 +22,11 @@ import { UserService } from './user.service';
 @Controller('user')
 @ApiTags('User')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService) { }
 
   @Post('/register')
   @HttpCode(200)
   @ApiOperation({ summary: '회원가입' })
-  @ApiResponse({
-    status: 200,
-    description: '회원가입 성공',
-    type: BaseResponse,
-  })
-  @ApiUnauthorizedResponse({
-    status: 401,
-    description: '이미 존재하는 아이디',
-    type: UnauthorizedResponse,
-  })
   async Register(@Body() registerDto: RegisterDto) {
     await this.userService.register(registerDto);
 
@@ -54,12 +39,6 @@ export class UserController {
   @Post('/login')
   @HttpCode(200)
   @ApiOperation({ summary: '로그인' })
-  @ApiResponse({ status: 200, description: '로그인 성공', type: LoginResponse })
-  @ApiResponse({
-    status: 404,
-    description: '존재하지 않는 유저',
-    type: BaseResponse,
-  })
   async Login(@Body() loginDto: LoginDto) {
     const token: string = await this.userService.login(loginDto);
 
@@ -75,18 +54,9 @@ export class UserController {
   @ApiOperation({ summary: '나의 정보 조회' })
   @ApiBearerAuth('access-token')
   @UseGuards(new PasswordGuard())
-  @ApiResponse({
-    status: 200,
-    description: '유저 조회 성공',
-    type: UserResponse,
-  })
-  async getMyInfo(@Token() user: User) {
+  async getMyInfo(@Token() user: User): Promise<BaseResponse<User>> {
     const data: User = await this.userService.getMyInfo(user.phone);
 
-    return {
-      data,
-      status: 200,
-      message: '나의 정보 조회 성공',
-    };
+    return new BaseResponse(200, "조회 성공", data);
   }
 }
